f3ad3384413dae0b12efd98fb7e21351dda7f97a
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-10-30 19:02:40 +0900

    feat: 오류 수정

--- PersonalLix/controllers/feedback_controller.py
+++ PersonalLix/controllers/feedback_controller.py
@@ -1,6 +1,6 @@
-import app
 from flask import Blueprint, request, jsonify, make_response
 from worker import update_data
+from extensions import task_queue  # extensions 모듈에서 task_queue 임포트
 
 feedback_bp = Blueprint('feedback', __name__)
 
@@ -12,6 +12,6 @@ def updade_model_req():
     if missing_fields:
         return make_response(jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400)
 
-    # Enqueue the update task
-    job = app.task_queue.enqueue(update_data, data)
+    # 작업 큐에 업데이트 작업 추가
+    job = task_queue.enqueue(update_data, data)  # app.task_queue 대신 task_queue 사용
     return make_response(jsonify({"info": f"Job submitted: {job.get_id()}"}), 200)
